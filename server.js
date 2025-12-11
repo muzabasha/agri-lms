824096579b016ea55d65a4b5e5b9935213cdcb93
muzamil basha <muzab@gkvk.edu>
2025-12-11 12:39:31 +0530

    Fix Vercel deployment issues: Updated server.js for static paths and added vercel.json

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '/')));
+app.use(express.static(process.cwd()));
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
